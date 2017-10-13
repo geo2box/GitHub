fbb1d730aa18ac714733a5900e83ce4e28f991e7
geo2box <32507997+geo2box@users.noreply.github.com>
2017-10-13 14:30:00 +0400

    Add files via upload

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Merci de votre visite !",
+    "resumePlaybackWording": "Relancer la lecture ?",
+    "fullscreenInfoWording": "Ce programme va se lancer en plein écran",
+    "yesWording": "Oui",
+    "title": "Agir pour le climat",
+    "noWording": "Non",
+    "analyticsKey": "",
+    "downloadAppWording": "Télécharger l'app",
+    "launchAppWording": "Et lancer le projet",
+    "thumbnail": "Medias/Photos/lemur-160899_1280.png",
+    "url": "",
+    "redirectToMobileApp": "never",
+    "description": "introduction du défi 2"
+}
